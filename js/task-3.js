103166a0d04c365c81f64dc1804ceeb4df857a3f
Yevhen Bozadzhi <ebozadji13@gmail.com>
2025-03-16 14:28:20 +0200

    done

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,11 @@
-function getElementWidth(content, padding, border) {
-    let contentWidth = parseFloat(content);
-    let paddingWidth = parseFloat(padding) * 2;
-    let borderWidth = parseFloat(border) * 2;
-
-    let elementWidth = contentWidth + paddingWidth + borderWidth;
-    return elementWidth
+function checkForSpam(message) {
+    const lowerCaseMessage = message.toLowerCase();
+    return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
 }
-console.log(getElementWidth("50px", "8px", "4px")); 
-console.log(getElementWidth("60px", "12px", "8.5px")); 
-console.log(getElementWidth("200px", "0px", "0px")); 
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
